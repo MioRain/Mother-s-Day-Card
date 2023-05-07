28e67186fe9db81d07e9e955bc7e1d383caadf55
MioRain <ericzero1593@hotmail.com>
2023-05-08 02:04:02 +0800

    refactor main.js

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
+let scene, camera, renderer
+
 function init() {
-  const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   )
-  camera.position.set(10, 10, 100)
+  camera.position.set(0, 0, 50)
+  camera.lookAt(scene.position)
   scene.add(camera)
 
   const axes = new THREE.AxesHelper(20)
   scene.add(axes)
 
-  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.setClearColor(0xffffff, 0.1);
-  renderer.render(scene, camera)
 
   document.body.append(renderer.domElement)
 }
 
+function render() {
+  renderer.render(scene, camera)
+}
+
 init()
+render()

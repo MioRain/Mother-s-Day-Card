29257d0642f5da7ab7ac5c1f8a0c0091d747f131
MioRain <ericzero1593@hotmail.com>
2023-05-08 02:25:56 +0800

    feat: add camera control

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let scene, camera, renderer
+let cameraControl
 
 function init() {
   scene = new THREE.Scene()
@@ -11,13 +12,17 @@ function init() {
   camera.position.set(0, 0, 50)
   camera.lookAt(scene.position)
   scene.add(camera)
-
+ 
   const axes = new THREE.AxesHelper(20)
   scene.add(axes)
-
+  
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.setClearColor(0xffffff, 0.1);
+  
+  cameraControl = new THREE.OrbitControls(camera, renderer.domElement)
+  cameraControl.enableDamping = true
+  cameraControl.dampingFactor = 0.05
 
   document.body.append(renderer.domElement)
 }
